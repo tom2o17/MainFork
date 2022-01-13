c74c4c7580eb4ec18aed19f450efa4627d2ce6d9
Thomas Clement <Thomasaclement98@gmail.com>
2022-01-12 21:55:48 -0500

    Update testERC20.js

--- test/testERC20.js
+++ test/testERC20.js
@@ -14,11 +14,16 @@ contract('IERC20', (accounts) => {
 });
 
 contract('AAVE', (accounts) => {
-
+  // https://docs.aave.com/developers/the-core-protocol/protocol-data-provider#getreserveconfigurationdata
   it("connect to the AAVE lending pool", async () => {
     const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
     const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
     const poolInstance = await aaveLendingPool.at("0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9");
-    console.log(`${ (await poolInstance.getReserveData("0xdAC17F958D2ee523a2206206994597C13D831ec7"))[1]}`);  
+    // Fetches the interest on the AAVE Polygon chain. The deposit APY is at the 3rd index position
+
+    var depositUSDTApyPolygon = (await poolInstance.getReserveData("0xdAC17F958D2ee523a2206206994597C13D831ec7"))[3]
+    var interestUSDTApyPolygon = (await poolInstance.getReserveData("0xdAC17F958D2ee523a2206206994597C13D831ec7"))[3]
+    
+    console.log(depositApy);  
   });
 });
